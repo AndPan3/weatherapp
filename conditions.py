3f11b937ac4807e36e100299c83f2318b3313f28
AndPan3 <andrejbajusic2012@gmail.com>
2026-06-20 15:05:51 +0200

    made code for badiwetter

--- conditions.py
+++ conditions.py
@@ -4,4 +4,11 @@ import json
 with open("data/weather1.json", "r") as file:
     data = json.load(file)
 
-print(data)
+weathernow = data[0]
+
+if weathernow.temparature_25 >= 26 and weathernow.apparent_temparature >= 25:
+    if weathernow.weather_code in [0, 1, 2]:
+        badiwetter = True
+else:
+    badiwetter=False
+
