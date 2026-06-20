c24a9941bc6366aee0535a8d18be8f2ed2abd128
AndPan3 <andrejbajusic2012@gmail.com>
2026-06-20 15:13:34 +0200

    Update weather condition checks and add conditions.json file

--- conditions.py
+++ conditions.py
@@ -8,7 +8,18 @@ weathernow = data[0]
 
 if weathernow.temparature_25 >= 26 and weathernow.apparent_temparature >= 25:
     if weathernow.weather_code in [0, 1, 2]:
-        badiwetter = True
+        badiwetter = "true"
 else:
-    badiwetter=False
+    badiwetter="false"
 
+if weathernow.rain >= 2:
+    regenwetter="true"
+else:
+    regenwetter="false"
+
+conditions = {
+    "badi": badiwetter,
+    "regen" : regenwetter
+}
+
+print(conditions)
