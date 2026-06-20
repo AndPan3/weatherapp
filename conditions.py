579fc98f693e6e28bf2d0439eb93e8812435ebdd
AndPan3 <andrejbajusic2012@gmail.com>
2026-06-20 15:21:37 +0200

    fixed typo (26 -> 25) fixed formating in if loop copied code from fetch.py so it updates json

--- conditions.py
+++ conditions.py
@@ -1,18 +1,18 @@
 import json
-
+import pandas as pd
 
 with open("data/weather1.json", "r") as file:
     data = json.load(file)
 
 weathernow = data[0]
 
-if weathernow.temperature_2m >= 26 and weathernow.apparent_temparature >= 25:
+if weathernow['temperature_2m'] >= 25 and weathernow['apparent_temparature'] >= 25:
     if weathernow.weather_code in [0, 1, 2]:
         badiwetter = "true"
 else:
     badiwetter="false"
 
-if weathernow.rain >= 2:
+if weathernow['rain'] >= 2:
     regenwetter="true"
 else:
     regenwetter="false"
@@ -21,5 +21,6 @@ conditions = {
     "badi": badiwetter,
     "regen" : regenwetter
 }
-
-print(conditions)
+conditionsdataframe = pd.DataFrame(data = conditions)
+file_path = 'data/conditions.json'
+conditionsdataframe.to_json(file_path, orient="records", date_format="iso", indent=4)
