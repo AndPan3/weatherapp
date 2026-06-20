9d4ab9c1796549c305ebc4580dc3d4e6aee0769b
AndPan3 <andrejbajusic2012@gmail.com>
2026-06-20 16:47:24 +0200

    Refactor weather condition checks and update JSON output format
    ran fetch.py again and bugfixes in conditions.py

--- conditions.py
+++ conditions.py
@@ -1,26 +1,26 @@
 import json
-import pandas as pd
-
 with open("data/weather1.json", "r") as file:
     data = json.load(file)
 
 weathernow = data[0]
 
-if weathernow['temperature_2m'] >= 25 and weathernow['apparent_temparature'] >= 25:
-    if weathernow.weather_code in [0, 1, 2]:
-        badiwetter = "true"
+if weathernow['temperature_2m'] >= 25 and weathernow['apparent_temperature'] >= 25:
+    if weathernow['weather_code'] in [0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 11.0, 12.0, 13.0, 14.0, 15.0, 16.0, 17.0, 18.0, 19.0]:
+        badiwetter = True
 else:
-    badiwetter="false"
+    badiwetter=False
 
 if weathernow['rain'] >= 2:
-    regenwetter="true"
+    regenwetter=True
 else:
-    regenwetter="false"
+    regenwetter=False
 
 conditions = {
     "badi": badiwetter,
     "regen" : regenwetter
 }
-conditionsdataframe = pd.DataFrame(data = conditions)
-file_path = 'data/conditions.json'
-conditionsdataframe.to_json(file_path, orient="records", date_format="iso", indent=4)
+with open('data/conditions.json', 'w') as f:
+    json.dump(conditions, f, indent=4)
+print(badiwetter)
+print(weathernow['weather_code'])
+print(f"{weathernow['temperature_2m']} , pppp {weathernow['apparent_temperature']}")
